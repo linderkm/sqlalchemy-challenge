3f0738cb2a321fdb616b350c881566ac4c677ea0
linderkm <kevin.linder94@gmail.com>
2024-11-24 20:41:15 -0700

    adding references to code comments. updating README with references for app.py

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # sqlalchemy-challenge
+This repository contains code and resources for the Module 10 challenge, focused on Sqlalchemy and Flask. 
+
+All files are stored in the SurfsUp folder.
+
+The analysis portion of the assignment can be found in SurfsUp/climate.ipynb
+
+The Flask app portion of the assignment can be found in SurfUp/app.py
+
+All references for sources used during the completion of this assignment are contained in this README. The numbers cited in the code comments corresponds with numbers listed alongside sources in this document.
+
+
+
+
+
+# Flask App
+
+(1) Module 10; Lesson 3; Activity 10; app_solution.py
+
+(2) Module 10; Lesson 3; Activity 2; dates_solution.ipynb
+
+(3) Module 10; Lesson 3; Activity 1; joins_solution.ipynb
+
+(4) https://www.geeksforgeeks.org/python-validate-string-date-format/
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,3 +1,4 @@
+# import dependencies (1)
 from flask import Flask, jsonify
 from datetime import datetime, timedelta
 
@@ -9,30 +10,31 @@ from sqlalchemy import create_engine, func
 #################################################
 # Database Setup
 #################################################
-# reflect an existing database into a new model
+# reflect an existing database into a new model (1)
 engine = create_engine('sqlite:///Resources/hawaii.sqlite')
 Base = automap_base()
 
-# reflect the tables
+# reflect the tables (1)
 Base.prepare(autoload_with=engine)
 
-# Save references to each tables
+# Save references to each table (1)
 measurements = Base.classes.measurement
 stations = Base.classes.station
 
 #################################################
 # Flask Setup
 #################################################
-#Module10;Lesson 3; Activity 10; app_solution.py
+#create Flask app object (1)
 app = Flask(__name__)
-
 
 #################################################
 # Flask Routes
 #################################################
+
+#index route (1)
 @app.route("/")
 def homepage():
-    """List all available routes."""
+    #List all available routes.
     return(
         f"Available Routes:<br/>"
         f"/api/v.1.0/precipitation<br/>"
@@ -48,7 +50,7 @@ def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    # get the most recent date, and subtract 1 year to get two values 12-months apart.
+    # get the most recent date, and subtract 1 year to get two values 12-months apart. (2)
     latest_date = session.query(measurements.date).order_by(measurements.date.desc()).first()[0]
     latest_date_minus_one_year = (datetime.strptime(latest_date.replace("-", "/"), "%Y/%m/%d") - timedelta(days=365)).strftime("%Y-%m-%d")
 
@@ -66,7 +68,7 @@ def precipitation():
     for n in d:
         dict[n[0]]=n[1]
 
-    return jsonify(dict)
+    return jsonify(dict) # (1)
 
 
 @app.route("/api/v.1.0/stations")
@@ -74,10 +76,10 @@ def station():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    #adding all query targets to list
+    #adding all query targets to list (3)
     sel = [stations.station, stations.name, stations.latitude, stations.longitude, stations.elevation]
 
-    #query to retrieve station data
+    #query to retrieve station data (3)
     d = session.query(*sel).all()
 
     #end Python connection to db.
@@ -100,17 +102,17 @@ def station():
 def tobs():
     session= Session(engine)
 
-    # get the most recent date, and subtract 1 year to get two values 12-months apart.
+    # get the most recent date, and subtract 1 year to get two values 12-months apart. (2)
     latest_date = session.query(measurements.date).order_by(measurements.date.desc()).first()[0]
     latest_date_minus_one_year = (datetime.strptime(latest_date.replace("-", "/"), "%Y/%m/%d") - timedelta(days=365)).strftime("%Y-%m-%d")
 
-    # query to find the most active stations
+    # query to find the most active stations (2)
     station_count = session.query(stations.station,func.count(stations.station)). \
         group_by(stations.station). \
         order_by(func.count(stations.station).desc()). \
         all()
 
-    #query to find most recent 12 months of temperature (tobs) data from most active station
+    #query to find most recent 12 months of temperature (tobs) data from most active station (2)
     most_active_station_data = session.query(measurements.date,measurements.tobs).\
         filter(measurements.date >= latest_date_minus_one_year).\
         filter(measurements.station==station_count[0][0]).\
@@ -133,27 +135,23 @@ def tobs():
 @app.route("/api/v.1.0/<start_date>")
 def start_date(start_date):
     #try except block that manages behavior based on user input.
-    # If the user inputs the incorrect date format, a ValueError is raised.
+    # If the user inputs the incorrect date format, a ValueError is raised. (4)
     try:
-        # check if user input format matches db date format
-        # https: // www.geeksforgeeks.org / python - validate - string - date - format /
+        # check if user input format matches db date format (4)
         bool(datetime.strptime(start_date,"%Y-%m-%d"))
         # Create session (link) from Python to the DB
         session = Session(engine)
 
-        #query the db to determine the earliest and latest record
+        #query the db to determine the earliest and latest record (2)
         earliest_record = session.query(measurements.date).order_by(measurements.date).first()[0]
         latest_record = session.query(measurements.date).order_by(measurements.date.desc()).first()[0]
 
         #check to ensure user input occurs within the db records
         if start_date <= latest_record and start_date>= earliest_record:
 
-            #setting query parameters
-            sel = [func.min(measurements.tobs),
-                   func.max(measurements.tobs),
-                   func.avg(measurements.tobs)
-                   ]
-            #query db, filtering by records that occur at or after the date submitted in Request
+            #setting query parameters (3)
+            sel = [func.min(measurements.tobs), func.max(measurements.tobs), func.avg(measurements.tobs)]
+            #query db, filtering by records that occur at or after the date submitted in Request (3)
             d = session.query(*sel).filter(measurements.date >= start_date).all()[0]
             # creating dictionary variable with query data.
             values_dict = {'Minimum Recorded Temperature (f)':d[0],
@@ -169,6 +167,7 @@ def start_date(start_date):
         # end Python connection to db.
         session.close()
 
+    # raise error (4)
     except ValueError:
         return f"Incorrect date format. Request using format YYY-MM-DD"
 
@@ -192,12 +191,12 @@ def start_end_date(start,end_date):
             if end_date <= latest_record and end_date >= earliest_record:
                 # conditional to check if Request start date occurs before Request end date (e.g. prevent start = 2015 and end = 2014)
                 if start < end_date:
-                    # setting query parameters
+                    # setting query parameters (3)
                     sel = [func.min(measurements.tobs),
                            func.max(measurements.tobs),
                            func.avg(measurements.tobs)
                            ]
-                    # query db, filtering by records that occur between Request start and end date values.
+                    # query db, filtering by records that occur between Request start and end date values. (3)
                     d = session.query(*sel).filter(measurements.date >= start).filter(measurements.date <= end_date).all()[0]
                     # creating dictionary variable to store query data.
                     values_dict = {'Minimum Recorded Temperature (f)':d[0],
@@ -219,12 +218,12 @@ def start_end_date(start,end_date):
         #end Python connection to db.
         session.close()
 
-
+    # raise error (4)
     except ValueError:
         return f"Incorrect date format. Request using format YYY-MM-DD"
 
 
-#activate Flask api when python file is run.
+#activate Flask api when python file is run. (1)
 if __name__ == '__main__':
     app.run(debug=True)
 
